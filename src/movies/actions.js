dcf21fbff67ecc5c4ad27ca44b28f7ee1ba17aa1
pajmera <pajmera@thoughtworks.com>
2018-03-30 12:51:59 +0530

    change to service api

--- src/movies/actions.js
+++ src/movies/actions.js
@@ -23,12 +23,12 @@ const fetchMovies = () => {
   return async (dispatch) => {
     dispatch(fetchMoviesInProgress);
     try {
-      // const movies = await axios.get('http://localhost:9090/movies/now-showing')
-      const movies = {data: [{
-        id: 'asfasdfas',
-        name: 'Kabali',
-        experience: 'asfasdfag',
-      }]}
+      const movies = await axios.get('http://13.127.224.201:9090/movies/now-showing')
+      // const movies = {data: [{
+      //   id: 'asfasdfas',
+      //   name: 'Kabali',
+      //   experience: 'asfasdfag',
+      // }]}
       const moviesData = movies.data.map(movie => {
         const sluggedData = slug(changeCase.sentenceCase(movie.name), { lower: true });
         return {...movie, slug: sluggedData}
